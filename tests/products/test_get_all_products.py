697222fe412e9027785ae1c417d9fd17a4ceda41
Aleksandr Liubinskii <a.liubinskii@hexacore.io>
2022-12-21 16:13:21 +0400

    delete unused variable

--- tests/products/test_get_all_products.py
+++ tests/products/test_get_all_products.py
@@ -10,7 +10,6 @@ def test_get_all_products():
     # make the call
     request_helper = RequestsUtility()
     rs_api = request_helper.get(endpoint='products')
-    rs_json = rs_api.json()
 
     # verify status code of the call
     assert rs_api.status_code == 200, f'Expected status code 200 but actual {rs_api.status_code}'
