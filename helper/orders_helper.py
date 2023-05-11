3658a9ac83864506ffbe0aa7a770ad9559b40116
Aleksandr Liubinskii <a.liubinskii@hexacore.io>
2023-05-11 13:41:25 +0400

    add test_create_paid_order_new_customer

--- helper/orders_helper.py
+++ helper/orders_helper.py
@@ -2,6 +2,7 @@ import logging
 import os
 import json
 from helper.woo_api_utility import WooAPIUtility
+from data_access_object.order_dao import OrderDAO
 
 
 class OrdersHelper(object):
@@ -25,3 +26,35 @@ class OrdersHelper(object):
         rs_api = self.woo_helper.post(wc_endpoint='orders', params=payload, expected_status_code=201)
 
         return rs_api
+
+    def verify_order_is_created(self, order_json, expected_products, customer_id=0):
+        order_dao = OrderDAO()
+
+        # Verify response
+
+        assert order_json, f"Create order response is empty"
+        assert order_json['customer_id'] == customer_id, f"Expected Customer_id {customer_id} " \
+                                                             f"but get {order_json['customer_id']}"
+        len_items = len(order_json['line_items'])
+        expected_len = len(expected_products)
+        order_id = order_json['id']
+        assert len_items == expected_len, f"Expected only {expected_len} item in order get: {len_items}," \
+                                          f" Order id: {order_id}"
+
+        # verify db
+        order_item_from_response = order_json['line_items']
+        # logging.info(f"{order_item_from_response}")
+        item_from_response = order_item_from_response[0]['name']
+        len1 = len(order_item_from_response[0]['name'])
+        logging.info(f"{len1}")
+        logging.info(f"Item name from response: {item_from_response}")
+
+        order_items = order_dao.get_order_item_by_order_id(order_id=order_id)
+        # logging.info(f"Items from db: {order_items}")
+        items = [i for i in order_items if i['order_item_type'] == 'line_item']
+        item_from_db = items[0]['order_item_name']
+        logging.info(f"Items from db: {item_from_db}")
+
+        assert len(items) == 1, f"Expected only 1 item get: {len(items)}"
+        assert item_from_response == item_from_db, f"Order Items from response {item_from_response}" \
+                                                   f" not equal order items from db {item_from_db}"
--- tests/orders/test_create_order_smoke.py
+++ tests/orders/test_create_order_smoke.py
@@ -3,13 +3,13 @@ import pytest
 from data_access_object.order_dao import OrderDAO
 from data_access_object.products_dao import ProductsDAO
 from helper.orders_helper import OrdersHelper
+from helper.helper import Helper
 import logging
 
 
 @pytest.mark.tcid48
 def test_create_paid_order_guest_user():
     product_dao = ProductsDAO()
-    order_dao = OrderDAO()
     order_helper = OrdersHelper()
 
     # get a product from db
@@ -21,26 +21,37 @@ def test_create_paid_order_guest_user():
     info = {"line_items": [{"product_id": product_id, "quantity": 1}]}
     order_json = order_helper.create_order(additional_args=info)
     order_id = order_json['id']
+    products = order_json['line_items']
+    logging.info(f"Order id: {products}")
     logging.info(f"Order id: {order_id}")
 
-    # verify response
-    assert order_json, f"Create order response is empty"
-    assert order_json['customer_id'] == 0, f"customer_id not a guest should be 0 get: {order_json['customer_id']}"
-    len_items = len(order_json['line_items'])
-    assert len_items == 1, f"Expected only 1 item in order get: {len_items}, Order id: {order_json['id']}"
+    # Verify response and db
+    order_helper.verify_order_is_created(order_json, products, customer_id=0)
 
-    # verify db
-    order_item_from_response = order_json['line_items']
-    #logging.info(f"{order_item_from_response}")
-    item_from_response = order_item_from_response[0]['name']
-    logging.info(f"Item name from response: {item_from_response}")
 
-    order_items = order_dao.get_order_item_by_order_id(order_id=order_id)
-    #logging.info(f"Items from db: {order_items}")
-    items = [i for i in order_items if i['order_item_type'] == 'line_item']
-    item_from_db = items[0]['order_item_name']
-    logging.info(f"Items from db: {item_from_db}")
+@pytest.mark.tcid49
+def test_create_paid_order_new_customer():
+    product_dao = ProductsDAO()
+    order_dao = OrderDAO()
+    order_helper = OrdersHelper()
+    customer_helper = Helper()
 
-    assert len(items) == 1, f"Expected only 1 item get: {len(items)}"
-    assert item_from_response == item_from_db, f"Order Items from response {item_from_response}" \
-                                               f" not equal order items from db {item_from_db}"
+    # get a product from db
+    random_product = product_dao.get_random_product_from_db(quantity=1)
+    product_id = random_product[0]['ID']
+    logging.info(f"Random product id {product_id}")
+
+    # Create new customer
+    new_customer = customer_helper.create_customer().json()
+    new_customer_id = new_customer['id']
+
+    # make the call
+    info = {"line_items": [{"product_id": product_id, "quantity": 1}], "customer_id": new_customer_id}
+    order_json = order_helper.create_order(additional_args=info)
+    order_id = order_json['id']
+    products = order_json['line_items']
+    logging.info(f"Order id: {products}")
+    logging.info(f"Order id: {order_id}")
+
+    # Verify response and db
+    order_helper.verify_order_is_created(order_json, products, customer_id=new_customer_id)
